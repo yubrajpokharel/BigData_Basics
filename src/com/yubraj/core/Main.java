8befa817e460f59e1754782fb0086bac200adcc8
yubraj_pokharel <yubraj.pokharel555@gmail.com>
2016-05-26 00:50:22 -0500

    Final result

--- src/com/yubraj/core/Main.java
+++ src/com/yubraj/core/Main.java
@@ -1,10 +1,16 @@
 package com.yubraj.core;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * Created by yubraj_pokharel on 5/24/16.
+ *
+ * Please notice that there are only three files used in this program
+ * if you want to input more than 3 nodes then please create the no. of files aslo
+ * respectively. Add the files in Filepath array
  */
-public class Main {
 
+public class Main {
 
     static String[] FilePath = { "/home/yubraj_pokharel/big_data_Class/BigData_Classes/testData.txt",
                                  "/home/yubraj_pokharel/big_data_Class/BigData_Classes/testData2.txt",
@@ -12,11 +18,22 @@ public class Main {
                                 };
 
     public static void main(String[] args) throws IOException {
-        WordCount wc = new WordCount(4,3,FilePath);
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter the no. of node : ");
+        int node = sn.nextInt();
+        System.out.println("Enter the no. of reducer : ");
+        int reducer = sn.nextInt();
+
+        Operations o = new Operations();
+        o.ask_decision();
+
+        WordCount wc = new WordCount(reducer,node,FilePath);
         wc.manage_partition();
         wc.group_by_keys();
         wc.showReducedOutput();
     }
+
+
 
 
 }
