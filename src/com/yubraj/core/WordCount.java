a5ff983cf8e3f43e94a31b750267b0a9c1ff3030
yubraj_pokharel <yubraj.pokharel555@gmail.com>
2016-05-26 00:22:52 -0500

    Complete MapReduce using Java and Google MultiMap

--- src/com/yubraj/core/WordCount.java
+++ src/com/yubraj/core/WordCount.java
@@ -1,11 +1,11 @@
 package com.yubraj.core;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by 984886 on 5/25/2016.
@@ -17,7 +17,10 @@ public class WordCount {
 
     public String[] filePath;
     static List<List<KeyPair<String, Integer>>> kpList = new ArrayList<>();
-    static List<List<Group>> groupByPair = new ArrayList<>();
+    public static Multimap<Integer, KeyPair<String, Integer>> groups = ArrayListMultimap.create();
+    static List<KeyPair<String, Integer>> mapped_list = new ArrayList<KeyPair<String, Integer>>();
+    static List<List<Group>> final_pair = new ArrayList<>();
+
 
     public WordCount(int r, int m, String[] filePath) throws IOException {
         this.r = r;
@@ -29,15 +32,60 @@ public class WordCount {
 
     private void suffle_sort() throws IOException {
         for (int i = 0; i < m; i++) {
+            System.out.println("\n--------------------------------");
             System.out.println("Mapper " + i + " Output");
+            System.out.println("--------------------------------");
             MapData(filePath[i], i);
-            SortMappedData(i);
-            for (List<KeyPair<String, Integer>> kvp : kpList) {
+            for (KeyPair<String, Integer> kvp : kpList.get(i)) {
                 System.out.println(kvp.toString());
+            }
+            SortMappedData(i);
+            System.out.println("--------------------------------");
+        }
+    }
+
+    public void manage_partition() {
+
+        for (int mid = 0; mid < m; mid++) {
+            for(int rid = 0; rid < r; rid++){
+                //List<KeyPair<String, Integer>> groupList = new ArrayList<>();
+                System.out.println("From map "+mid+" to reducer "+rid);
+                for(KeyPair<String, Integer> kp : kpList.get(mid)){
+                    if(rid == getPartition(kp.getK())){
+                        groups.put(rid, kp);
+                        System.out.println(kp);
+                    }
+                }
             }
         }
     }
 
+    public void group_by_keys(){
+        List<List<KeyPair<String, Integer>>> list = new ArrayList<>();
+        for (Integer key : groups.keySet()) {
+            List<KeyPair<String , Integer>> list2 = (List<KeyPair<String, Integer>>) groups.get(key);
+            list.add(list2);
+        }
+        System.out.println("\n\n");
+        for(int i = 0; i<list.size(); i++){
+            System.out.println("--------------------------------");
+            System.out.println("Reducer Input "+ i);
+            System.out.println("--------------------------------");
+            GroupByPair(list.get(i));
+        }
+    }
+
+    public void showReducedOutput(){
+        System.out.println("\n\n");
+        for(int i = 0; i<final_pair.size(); i++){
+            System.out.println("--------------------------------");
+            System.out.println("Reducer "+i+ " output");
+            System.out.println("--------------------------------");
+            for(Group g: final_pair.get(i)){
+                System.out.println(Reducer(g));
+            }
+        }
+    }
 
 
     public int getPartition(String key){
@@ -47,27 +95,39 @@ public class WordCount {
 
     public static void MapData(String filePath, int index) throws IOException {
         String content = new String(Files.readAllBytes(Paths.get(filePath)));
-        String[] result = content.split("[-\\s]");
+        String[] result;
+        content = content.replaceAll("\"|\'", " ");
+        result = content.split("[-\\s]");
+
+        List<KeyPair<String, Integer>> kpListtemp = new ArrayList<>();
         for (String s : result) {
             if (s.matches("[a-zA-Z]+")) {
-
-                kpList.get(index).add((KeyPair<String, Integer>) new KeyPair<String, Integer>(s.toLowerCase(), 1));
+                KeyPair<String, Integer> newKeyPair = new KeyPair<>(s.toLowerCase(), 1);
+                kpListtemp.add(newKeyPair);
             }
         }
+        kpList.add(index, kpListtemp);
     }
 
-    public static void GroupByPair(int index) {
-        for (int i = 0; i < kpList.get(index).size(); i++) {
-            String currentKey = kpList.get(index).get(i).getK();
+    public static void GroupByPair(List<KeyPair<String, Integer>> list) {
+        Collections.sort(list, (l1, l2) -> l1.getK().compareTo(l2.getK()) );
+        List<Group> groupByPair = new ArrayList<Group>();
+
+        for (int i = 0; i < list.size(); i++) {
+            String currentKey = list.get(i).getK();
             Group gbp = new Group(currentKey);
 
-            while (i < kpList.size() - 1 && kpList.get(index).get(i + 1).getK().equals(currentKey)) {
+            while (i < list.size() - 1 && list.get(i + 1).getK().equals(currentKey)) {
                 gbp.addVal();
                 i++;
             }
-            groupByPair.get(index).add(gbp);
+            groupByPair.add(gbp);
         }
+        final_pair.add(groupByPair);
+        groupByPair.forEach(System.out::println);
+
     }
+
 
     public static void SortMappedData(int i) {
         Collections.sort(kpList.get(i), (a, b) -> a.getK().compareToIgnoreCase(b.getK()));
