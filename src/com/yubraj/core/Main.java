7b2c6cc3d13a916158d16e9d27452217afafe899
yubraj_pokharel <yubraj.pokharel555@gmail.com>
2016-05-26 00:54:40 -0500

    Final result

--- src/com/yubraj/core/Main.java
+++ src/com/yubraj/core/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Created by yubraj_pokharel on 5/24/16.
  *
  * Please notice that there are only three files used in this program
- * if you want to input more than 3 nodes then please create the no. of files aslo
+ * if you want to input more than 3 mapper then please create the no. of files aslo
  * respectively. Add the files in Filepath array
  * Output can be seen the Output.txt file in the root folder if you type yes in the console
  */
@@ -20,14 +20,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner sn = new Scanner(System.in);
-        System.out.println("Enter the no. of node : ");
+        System.out.println("Enter the no. of mapper : ");
         int node = sn.nextInt();
         System.out.println("Enter the no. of reducer : ");
         int reducer = sn.nextInt();
 
         Operations o = new Operations();
         o.ask_decision();
-
+        System.out.println("total Mapper : "+node);
+        System.out.println("total Reducer : "+reducer);
         WordCount wc = new WordCount(reducer,node,FilePath);
         wc.manage_partition();
         wc.group_by_keys();
